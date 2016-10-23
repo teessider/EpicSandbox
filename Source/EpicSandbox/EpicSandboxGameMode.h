efc4ff9b11b0683de441ad85b3e8e76b3d5026d7
teessider <andrew@andybellart.co.uk>
2016-10-23 16:22:33 +0200

    Initial commit

---
+++ Source/EpicSandbox/EpicSandboxGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "EpicSandboxGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICSANDBOX_API AEpicSandboxGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
