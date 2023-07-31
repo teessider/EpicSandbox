dcc922a165b2a3897fe0091298ae0111333feb51
teessider <andrew@techartist.lol>
2023-07-31 02:09:41 +0200

    Changed the MyCustomAsset Class Icon & Thumbnail to the new SVG image brushes (they are currently using the Engine Data Asset Icon which now has an SVG version). - Added an extra comment about Toolbar icon sizes

--- Source/EpicSandboxEditor/Private/EpicSandboxEditorStyle.cpp
+++ Source/EpicSandboxEditor/Private/EpicSandboxEditorStyle.cpp
@@ -4,10 +4,13 @@
 
 #include "Styling/SlateStyle.h"
 #include "Styling/SlateStyleRegistry.h"
+#include "Styling/SlateStyleMacros.h"
+
+// Used as part of the IMAGE_BRUSH & IMAGE_BRUSH_SVG Macros from SlateStyleMacros!
+// Hence why this is here as opposed to basically duplicating the existing macros...
+#define RootToContentDir EditorStyle->RootToContentDir
 
 TSharedPtr<FSlateStyleSet> FEpicSandboxEditorStyle::EditorStyle = nullptr;
-
-#define IMAGE_BRUSH(RelativePath, ... ) FSlateImageBrush(EditorStyle->RootToContentDir( RelativePath, TEXT(".png") ), __VA_ARGS__ )
 
 const FVector2D Icon16x16(16.0f, 16.0f);
 const FVector2D Icon64x64(64.0f, 64.0f);
@@ -24,18 +27,18 @@ void FEpicSandboxEditorStyle::Initialize()
 
 	// For now though the default one(s) are used (the editor folders)
 	//F:\SandboxEngine\Engine\Content\Editor\Slate\Icons\AssetIcons <- location of editor icons
+	//F:\SandboxEngine\Engine\Content\Editor\Slate\Starship\AssetIcons <- location of new SVG icons
 	EditorStyle->SetContentRoot(FPaths::EngineContentDir() / TEXT("Editor/Slate"));
 	EditorStyle->SetCoreContentRoot(FPaths::EngineContentDir() / TEXT("Slate"));
 
 	// Class Icons are 16x16
 	// Class Thumbnails are 64x64
 	// 
-	// Toolbar Small Icons are 20x20
-	// Toolbar Icons are 40x40
+	// Toolbar Icons are 20x20
 
 	// MyCustomAsset style
-	EditorStyle->Set("ClassIcon.MyCustomAsset",	new IMAGE_BRUSH("Icons/AssetIcons/DataAsset_16x", Icon16x16));
-	EditorStyle->Set("ClassThumbnail.MyCustomAsset", new IMAGE_BRUSH("Icons/AssetIcons/DataAsset_64x", Icon64x64));
+	EditorStyle->Set("ClassIcon.MyCustomAsset",	new IMAGE_BRUSH_SVG("Starship/AssetIcons/DataAsset_16", Icon16x16));
+	EditorStyle->Set("ClassThumbnail.MyCustomAsset", new IMAGE_BRUSH_SVG("Starship/AssetIcons/DataAsset_64", Icon64x64));
 	// EditorStyle->Set("AssetColor.MyCustomAsset", FColor::Emerald); // Could define this here as part of the style but would need a TSharedRef to the ISlateStyleSet version of EditorStyle
 			
 	FSlateStyleRegistry::RegisterSlateStyle(*EditorStyle.Get()); // Make sure to register the style!
