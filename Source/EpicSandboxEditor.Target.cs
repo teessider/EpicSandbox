4b7377a483772411a83a109a993a47d75cc8c21c
teessider <andrew@techartist.lol>
2023-11-07 02:05:53 +0100

    Upgraded DefaultBuildSettings of EpicSandboxEditor Target to latest numbered version (V4)

--- Source/EpicSandboxEditor.Target.cs
+++ Source/EpicSandboxEditor.Target.cs
@@ -8,7 +8,7 @@ public class EpicSandboxEditorTarget : TargetRules
 	public EpicSandboxEditorTarget(TargetInfo Target) : base (Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;  // needed to avoid backwards compat mode
+		DefaultBuildSettings = BuildSettingsVersion.V4;  // needed to avoid backwards compat mode
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.AddRange( new[] { "EpicSandbox", "EpicSandboxEditor", "MyCustomAssetEditor" } );
 	}
