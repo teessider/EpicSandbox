bcffb834751d7c92fc5521d6ed884937345ae8ab
teessider <andrew@techartist.lol>
2026-04-19 13:41:37 +0200

    Put some duplicate code in the asset registry tags into a macro only meant for that function

--- Source/EpicSandbox/Private/MyCustomAsset.cpp
+++ Source/EpicSandbox/Private/MyCustomAsset.cpp
@@ -15,12 +15,14 @@ void UMyCustomAsset::GetAssetRegistryTags(FAssetRegistryTagsContext Context) con
 	Super::GetAssetRegistryTags(Context);
 	
 #if WITH_EDITORONLY_DATA
-	Context.AddTag(FAssetRegistryTag("FirstCustomMesh", FirstCustomStaticMesh ? TEXT("UObject Name ") + FirstCustomStaticMesh->GetName() : TEXT("None"), FAssetRegistryTag::TT_Alphabetical));
-	Context.AddTag(FAssetRegistryTag("SecondCustomMesh", SecondCustomStaticMesh ? TEXT("UObject FName (Logical Name) ") + SecondCustomStaticMesh->GetFName().ToString() : TEXT("None"), FAssetRegistryTag::TT_Alphabetical));
+#define GET_NAME(Object) (Object ? Object->GetName() : TEXT("None"))
+	Context.AddTag(FAssetRegistryTag("FirstCustomMesh", GET_NAME(FirstCustomStaticMesh), FAssetRegistryTag::TT_Alphabetical));
+	Context.AddTag(FAssetRegistryTag("SecondCustomMesh", GET_NAME(SecondCustomStaticMesh), FAssetRegistryTag::TT_Alphabetical));
 	// Context.AddTag(FAssetRegistryTag("CustomInt", FString::FromInt(CustomInt), FAssetRegistryTag::TT_Numerical));  // Alternative to AssetRegistrySearchable UPROPERTY metadata
 	Context.AddTag(FAssetRegistryTag("IsActive", bIsActive ? TEXT("True") : TEXT("False"), FAssetRegistryTag::TT_Alphabetical));
 	Context.AddTag(FAssetRegistryTag("Description", Description.IsEmpty() ? TEXT("Empty") : Description, FAssetRegistryTag::TT_Alphabetical));
-	Context.AddTag(FAssetRegistryTag("CustomDataAsset", CustomDataAsset ? CustomDataAsset->GetName() : TEXT("None"), FAssetRegistryTag::TT_Alphabetical));
+	Context.AddTag(FAssetRegistryTag("CustomDataAsset", GET_NAME(CustomDataAsset), FAssetRegistryTag::TT_Alphabetical));
+#undef GET_NAME
 #endif
 }
 
